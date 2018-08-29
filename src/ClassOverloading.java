fd9860e066c6d50ed86f2d33344b71695b465ead
Thomas <thomas.weber.2021@student.lhschools.org>
2018-08-29 14:17:36 -0400

    Time of Day demo, added basic error checking.

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class ClassOverloading {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many hours?");
+        int hr = sc.nextInt();
+        System.out.println("How many minutes?");
+        int min = sc.nextInt();
+        TimeOfDay now = new TimeOfDay(hr,min);
+        if(TimeOfDay.chkLegality(now.getMhr(),now.getMmin())){System.out.println(now.getMhr()+":"+now.getMmin());}
+        else{System.out.println("This time is invalid.");}
+    }
 }
--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -1,2 +1,38 @@
 public class TimeOfDay {
+    private int mhr;
+    private int mmin;
+
+    public TimeOfDay(int mhr) {
+        if(chkLegality(mhr,0)==true){
+            this.mhr=mhr;
+            this.mmin=0;
+        }
+        else{System.err.println("Invalid minute.");}
+    }
+
+    public TimeOfDay(int mhr, int mmin) {
+        if(chkLegality(mhr,mmin)==true){
+            this.mhr = mhr;
+            this.mmin = mmin;
+        }
+        else{System.err.println("Invalid time.");}
+    }
+
+    public TimeOfDay() {
+        this(0,0); // already legal
+    }
+
+    public int getMhr() {
+        return mhr;
+    }
+
+    public int getMmin() {
+        return mmin;
+    }
+
+    public static boolean chkLegality(int hr, int min){
+        if(hr<0||hr>23){return false;}
+        if(min<0||min>59){return false;}
+        return true;
+    }
 }
