921ee2174ea82838cd35fbad6c3d2249033779bc
Thomas <thomas.weber.2021@student.lhschools.org>
2018-09-04 13:26:50 -0400

    Finish Constructor Overloading, make time difference class

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -3,16 +3,23 @@ public class ClassOverloading {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        boolean done = false;
+        while(!done){
         System.out.println("How many hours?");
         int hr = sc.nextInt();
         System.out.println("How many minutes?");
-        try {
-            int min = sc.nextInt();
-            TimeOfDay now = new TimeOfDay(hr, min);
-            System.out.println(now.getMhr() + ":" + now.getMmin());
-        }
-        catch(IllegalArgumentException e){
-            System.err.println(e.getMessage());
-        }
+        int min = sc.nextInt();
+        if(TimeOfDay.chkLegality(hr,min)) {
+            try {
+                TimeOfDay now = new TimeOfDay(hr, min);
+                System.out.println(now.getMhr() + ":" + now.getMmin());
+                done=true;
+              }
+            catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+              }
+         }
+        else{System.out.println("Invalid time.");}
+      }
     }
 }
---
+++ src/TimeDifference.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TimeDifference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean inputA=false;
+        while (!inputA) {
+            //TODO
+        }
+    }
+
+
+}
